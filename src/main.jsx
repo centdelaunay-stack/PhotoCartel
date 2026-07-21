589f9cdc600014f8b7687444c20705db2ff74415
Vincent DELAUNAY <cent.delaunay@wanadoo.fr>
2026-07-21 08:09:36 +0200

    v42.0.8 – Correction Dernières visites Android 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/sw.js")
-      .then(() => {
-        console.log("PhotoCartel service worker enregistré.");
-      })
-      .catch((error) => {
-        console.error("Erreur service worker PhotoCartel :", error);
-      });
-  });
-}
+);
