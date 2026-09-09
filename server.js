e885338ea9551205ed3d29954d9ea741fc49d105
Vincent DELAUNAY <cent.delaunay@wanadoo.fr>
2026-09-09 07:23:10 +0200

    v74 – optimisation chargement photos analysees 5

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ import { exec } from "child_process";
 dotenv.config();
 
 const app = express();
-const VERSION_PHOTOCARTEL = "v73";
+const VERSION_PHOTOCARTEL = "v74";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
